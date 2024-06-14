892013b9e8b74d642058ad30861585c6614a03de
Rupak Poddar <144166964+rupakpoddaradmt@users.noreply.github.com>
2024-06-14 16:23:22 -0400

    Add files via upload

--- HumanSonotronWeb/flutter_service_worker.js
+++ HumanSonotronWeb/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "8ea08ea2444cc58ec5807fd7669e488f",
+"assets/fonts/MaterialIcons-Regular.otf": "525ed721f3000ad22f335c8422d08d58",
 "assets/NOTICES": "0a8d05c8b1869580511ed2082285c3f6",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1439dad717d3745cf62a77904dcf02ca",
-"/": "1439dad717d3745cf62a77904dcf02ca",
-"main.dart.js": "5552dd69f6924c96e94bc280d8806006",
+"index.html": "b1f8bc135141b411c7448427be1987fd",
+"/": "b1f8bc135141b411c7448427be1987fd",
+"main.dart.js": "467f11cc14e0f2c64731e1d827b5167f",
 "manifest.json": "617565b0b7c9e29ca5c39f8e0c611171",
 "version.json": "22951643a5e5a9c7b8ca6594074b26f6"};
 // The application shell files that are downloaded before a service worker can
